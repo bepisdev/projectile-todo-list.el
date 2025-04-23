f26838c9aa95645bf0da73c3a0a34871fc14d04c
Josh Burns <joshyburnss@gmail.com>
2025-04-23 13:06:20 +1000

    define functions to identify line-comments in a given file

--- projectile-todo-list.el
+++ projectile-todo-list.el
@@ -33,6 +33,35 @@
   :type '(repeat string)
   :group 'projectile-todo-list)
 
+(defcustom projectile-todo-list-comment-markers
+  '((python-mode . "#")
+    (emacs-lisp-mode . ";;")
+    (js-mode . "//")
+    (typescript-mode . "//")
+    (go-mode . "//")
+    (c-mode . "//")
+    (c++-mode . "//")
+    (ruby-mode . "#"))
+  "Alist mapping major modes to comment line markers. Used as a fallback, in case you're having issues with the automatic detection. You can expand the alist."
+  :type '(alist :key-type symbol :value-type string)
+  :group 'projectile-todo-list)
+
+(defvar projectile-todo-list-results-buffer "*projectile-todo-list"
+  "Name of the buffer used to display results.")
+
+(defun projectile-todo-list--get-project-files ()
+  "Return the file tree of the current project."
+  (if (projectile-project-p)
+      (mapcar (lambda (f) (expand-file-name f (projectile-project-root)))
+	      (projectile-current-project-files))
+    (error "Not in a valid Projectile project")))
+
+(defun projectile-todo-list--line-starts-with-comment (line mode)
+  "Return non-nil if LINE starts with the comment marker for MODE."
+  (let ((marker (cdr (assoc mode projectile-todo-list-comment-markers))))
+    (when marker
+      (string-match-p (concat "^\\s-*" (regexp-quote marker)) line))))
+
 (provide 'projectile-todo-list)
 
 ;;; projectile-todo-list.el ends here
